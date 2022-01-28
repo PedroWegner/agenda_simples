2014470168f2b6829eaf90fded58f4299f441457
unknown <pedrowegner@gmail.com>
2022-01-28 13:41:55 -0300

    implementada conexao para adicionar e ler banco

--- agenda_simples.py
+++ agenda_simples.py
@@ -17,10 +17,50 @@ def conecta():
         cursorclass=pymysql.cursors.DictCursor
     )
 
+    try:
+        yield conn
+    finally:
+        conn.close()
+
 
 if __name__ == '__main__':
     try:
         conecta()
         print('Conexão sucesso')
+
+        # crud simples
+        # adiciona contato no banco
+        # with conecta() as conexao:
+        #     with conexao.cursor() as cursor:
+        #         sql_comando = 'INSERT INTO contato (nome_contato, sobrenome_contato, email_contato, celular) VALUES' \
+        #                       '(%s, %s, %s, %s)'
+        #         cursor.execute(sql_comando, ('Pedro', 'Wegner', 'pedrowegner', '41 99672-2273'))
+        #         conexao.commit()
+
+        # le contato no banco
+        with conecta() as conexao:
+            with conexao.cursor() as cursor:
+                sql_comando = 'SELECT * FROM contato'
+                cursor.execute(sql_comando)
+                select = cursor.fetchall()
+
+                for linha in select:
+                    print(linha)
+
+        #
+        # # atualiza contato no banco
+        # with conecta() as conexao:
+        #     with conexao.cursor() as cursor:
+        #         sql_comando = ''
+        #         cursor.execute(sql_comando)
+        #         conexao.commit()
+        #
+        # # exclui contato no banco
+        # with conecta() as conexao:
+        #     with conexao.cursor() as cursor:
+        #         sql_comando = ''
+        #         cursor.execute(sql_comando)
+        #         conexao.commit()
+
     except Exception as e:
-        print(f'Não possível conectar ao banco. Erro: {e}')
+        print(f'{e}')
